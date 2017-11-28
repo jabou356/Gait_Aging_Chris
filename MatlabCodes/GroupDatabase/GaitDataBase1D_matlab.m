8dc855e40c0a300dfd9bc45ba289df352c76184e
Unknown <jason.bouffard@mail.mcgill.ca>
2017-11-27 22:49:54 -0500

    Build matlab Database working

--- MatlabCodes/GroupDatabase/GaitDataBase1D_matlab.m
+++ MatlabCodes/GroupDatabase/GaitDataBase1D_matlab.m
@@ -1,45 +1,25 @@
 clear; clc;
-GenericPathRPT
+GenericPathGait
 
-temp=importdata([Path.GroupDataPath, 'MegaDatabase1D.txt']);
+[~,~,temp]=xlsread([Path.GroupDataPath, 'EMG_spatialcurves.xlsx'],2);
 
-k=1;
-for iline=size(temp.data,1):-1:1
+
+for iline=size(temp,1):-1:2
     
-    Megadatabase(iline).Subject = temp.textdata(iline+1,1);
-    Megadatabase(iline).SID = str2double(temp.textdata(iline+1,2));
-    Megadatabase(iline).Time = str2double(temp.textdata(iline+1,3));
-    Megadatabase(iline).Project = temp.textdata(iline+1,4);
-    Megadatabase(iline).Endurance = str2double(temp.textdata(iline+1,5));
-    Megadatabase(iline).Sex = temp.textdata(iline+1,6);
-    Megadatabase(iline).Age = str2double(temp.textdata(iline+1,7));
-    Megadatabase(iline).Height = str2double(temp.textdata(iline+1,8));
-    Megadatabase(iline).Weight = str2double(temp.textdata(iline+1,9));
-    Megadatabase(iline).Signal = temp.textdata(iline+1,10);
-    Megadatabase(iline).Stat = temp.textdata(iline+1,11);
+    Megadatabase(iline-1).Subject = temp(iline,1);
+    Megadatabase(iline-1).SID = temp(iline,2);
+    Megadatabase(iline-1).Sex = temp(iline,3);
+    Megadatabase(iline-1).Age = temp(iline,4);
+    Megadatabase(iline-1).Height = temp(iline,5);
+    Megadatabase(iline-1).Weight = temp(iline,6);
+    Megadatabase(iline-1).Stat = temp(iline,7);    
+    Megadatabase(iline-1).Muscle = temp(iline,8);
+
     
-    Megadatabase(iline).Forward = temp.data(iline,1:100);    
-    Megadatabase(iline).Backward = temp.data(iline,101:200);
-    
+    Megadatabase(iline-1).data = temp(iline,9:109);        
     
 end
 
-subjects=unique(arrayfun(@(x)(x.Subject),Megadatabase));
 
-for iline=size(temp.data,1):-1:1
-
-    Megadatabase(iline).SubjectID = find(strcmp(subjects,Megadatabase(iline).Subject));
-    
-    if strcmp(Megadatabase(iline).Sex,'M')
-        
-        Megadatabase(iline).Sex=1;
-        
-    elseif strcmp(Megadatabase(iline).Sex,'F')
-        
-        Megadatabase(iline).Sex=2;
-        
-    end
-    
-end
 
 save([Path.JBAnalyse, 'GroupData1D.mat'], 'Megadatabase');
